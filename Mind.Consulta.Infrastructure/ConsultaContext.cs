1192dceaa992c09d9fbbc7b8a9930de9df2bd0ed
Administrator <Administrator@PERTH>
2016-10-11 20:17:36 -0300

    #save

---
+++ Mind.Consulta.Infrastructure/ConsultaContext.cs
@@ -0,0 +1,39 @@
+﻿using Mind.Consulta.Domain.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mind.Consulta.Infrastructure
+{
+    public class ConsultaContext: DbContext
+    {
+        public ConsultaContext():base("ConsultaDB")
+        {
+
+        }
+
+        public DbSet<Beneficiario> Beneficiarios { get; set; }
+
+        public DbSet<Cidade> Cidades { get; set; }
+
+        public DbSet<Consulta.Domain.BusinessObject.Consulta> Consultas { get; set; }
+
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        public DbSet<Especialidade> Especialidades { get; set; }
+
+        public DbSet<Estado> Estados { get; set; }
+
+        public DbSet<Medico> Medicos { get; set; }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public DbSet<PessoaFisica> PessoaFisicas { get; set; }
+
+
+
+    }
+}
