d33d515638f470569267c2ff01f7b6b2c0523a8a
Administrator <Administrator@PERTH>
2016-10-11 22:14:37 -0300

    #change

---
+++ Mind.Consulta.TestIntegration/Repository/BeneficiarioRepoSpec.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mind.Consulta.Test.Integration
+{
+    [TestClass]
+    public class BeneficiarioRepoSpec
+
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
